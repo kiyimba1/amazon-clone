39258b1bc21bd1a340a89d87bfba1f2daee25d34
kiyimba1 <edwineddie54@gmail.com>
2021-04-10 11:31:25 +0300

    --- Auto-Git Commit ---

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+        //some fancy firbase login shiiiiiiiiiiiiiiiittttttttt...........
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -12,10 +20,10 @@ function Login() {
                 <h1>Sign in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' />
+                    <input type='text' value={email} onChange= {e => setEmail(e.target.value)} />
                     <h5>Password</h5>
-                    <input type='password' />
-                    <button className='login__signInButton'>Sign In</button>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <button type='submit' className='login__signInButton' onClick={signIn}>Sign In</button>
                 </form>
                 <p>
                     By signing in  you agree to Amazon's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads.
