df0445486f74e29fa000ccd53400b5cc64ddb4b9
kiyimba1 <edwineddie54@gmail.com>
2021-04-12 19:23:24 +0300

    --- Auto-Git Commit ---

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 
 function Payment() {
-    const [{ basket, user }, dispatch] = useStateValue;
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className="payment">
             <div className="payment__container">
@@ -22,10 +23,24 @@ function Payment() {
                         <h3>Review items and Delivery</h3>
                     </div>
                     <div className='payment__item'>
-                        
+                        {basket.map(item =>
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        )}
                     </div>
                 </div>
                 <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="">
+                        
+                    </div>
 
                 </div>
             </div>
