9058f165b7fa0487160e377a3d57934e59ed2106
kiyimba1 <edwineddie54@gmail.com>
2021-04-14 22:55:10 +0300

    Order Page styling

--- src/Order.js
+++ src/Order.js
@@ -19,7 +19,7 @@ function Order({ order }) {
                     image={item.image}
                     price={item.price}
                     rating={item.rating}
-                // hideButton
+                    hideButton
                 />
             ))}
             <CurrencyFormat
